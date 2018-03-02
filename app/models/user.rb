9dd96b2342aa8d1d303f80a0043d9d27f7463dd6
Reid South <rbsouth@gmail.com>
2018-03-02 16:32:02 -0700

    user can add an account and edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
+	has_many :boats
+	
+	validates :email, presence: true, uniqueness: true
+	validates :password, presence: true, length: {minimum: 6}
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :company, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :boats
+ 
 end
