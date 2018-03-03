193d2c20f7b2ada53b5ab8636ae5a1fe9ee24674
Reid South <rbsouth@gmail.com>
2018-03-03 01:09:19 -0700

    added assignments table

---
+++ app/models/assignment.rb
@@ -0,0 +1,4 @@
+class Assignment < ApplicationRecord
+  belongs_to :boat
+  belongs_to :job
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_03_011322) do
+ActiveRecord::Schema.define(version: 2018_03_03_072926) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2018_03_03_011322) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "assignments", force: :cascade do |t|
+    t.integer "boat_id"
+    t.integer "job_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["boat_id"], name: "index_assignments_on_boat_id"
+    t.index ["job_id"], name: "index_assignments_on_job_id"
   end
 
   create_table "boats", force: :cascade do |t|
